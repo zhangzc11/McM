74dfeb34d9424b09da168b8a4d5d551ca1ba75d0
Zhicai <zzhang2@caltech.edu>
2017-08-08 13:55:36 +0200

    add config for Neutralino to Photon Gravitino production

---
+++ Configuration/GenProduction/python/ThirteenTeV/NeutralinoNeutralinoToGravitinoGravitinoPhotonPhoton_Tau5000mm_13TeV-pythia8_cff.py
@@ -0,0 +1,189 @@
+MNEU = 1000.0 # neutrino mass in GeV
+CTAU = 5000  # neutrino lifetime, this is in cm, this is 500=1.7 ns
+
+MGRA = 1 # set the mass of the gravitino
+WIDTH = 0.0197e-11 / CTAU
+
+SLHA_TABLE = '''
+BLOCK SPINFO      # Spectrum calculator information
+    1    Minimal  # spectrum calculator
+    2    1.0.0    # version number
+
+BLOCK MODSEL      # Model selection
+    1    1        #
+
+Block MINPAR  # Input parameters
+    3    1.00000000E+00  # tanb at m_Z   
+#
+Block SMINPUTS  # SM parameters
+         1     1.27931277E+02  # alpha_em^-1(MZ)^MSbar
+         2     1.16639000E-05  # G_mu [GeV^-2]
+         3     1.17200000E-01  # alpha_s(MZ)^MSbar
+         4     9.11876000E+01  # m_Z(pole)
+         5     4.20000000E+00  # m_b(m_b), MSbar
+         6     1.74300000E+02  # m_t(pole)
+         7     1.77700000E+00  # m_tau(pole)
+
+BLOCK MASS        # Mass Spectrum
+#   PDG code   mass  particle
+        24     8.04009772E+01  # W+
+        25     125  # h0
+        35     10.0E+4  # H0
+        36     10.0E+4  # A0
+        37     10.0E+4  # H+
+   1000001     10.0E+4  # ~d_L
+   2000001     10.0E+4  # ~d_R
+   1000002     10.0E+4  # ~u_L
+   2000002     10.0E+4  # ~u_R
+   1000003     10.0E+4  # ~s_L
+   2000003     10.0E+4  # ~s_R
+   1000004     10.0E+4  # ~c_L
+   2000004     10.0E+4  # ~c_R
+   1000005     10.0E+4  # ~b_1
+   2000005     10.0E+4  # ~b_2
+   1000006     10.0E+4  # ~t_1 
+   2000006     10.0E+4  # ~t_2
+   1000011     10.0E+4  # ~e_L-
+   2000011     10.0E+4  # ~e_R-
+   1000012     10.0E+4  # ~nu_eL
+   1000013     10.0E+4  # ~mu_L-
+   2000013     10.0E+4  # ~mu_R-
+   1000014     10.0E+4  # ~nu_muL
+   1000015     10.0E+4  # ~tau_1-
+   2000015     10.0E+4  # ~tau_2-
+   1000016     10.0E+4  # ~nu_tauL
+   1000021     10.0E+4  # ~g 
+   1000022     1000.0    # ~chi_10 
+   1000023     10.0E+4  # ~chi_20 
+   1000025     10.0E+4  # ~chi_30
+   1000035     10.0E+4  # ~chi_40
+   1000024     10.0E+4  # ~chi_1+ 
+   1000037     10.0E+4  # ~chi_2+
+   1000039     1# ~G
+#
+BLOCK NMIX  # Neutralino Mixing Matrix
+  1  1     9.79183656E-01   # N_11
+  1  2    -8.70017948E-02   # N_12
+  1  3     1.75813037E-01   # N_13
+  1  4    -5.21520034E-02   # N_14
+  2  1     1.39174513E-01   # N_21
+  2  2     9.44472080E-01   # N_22
+  2  3    -2.71658234E-01   # N_23
+  2  4     1.21674770E-01   # N_24
+  3  1    -7.50233573E-02   # N_31
+  3  2     1.16844446E-01   # N_32
+  3  3     6.87186106E-01   # N_33
+  3  4     7.13087741E-01   # N_34
+  4  1    -1.27284400E-01   # N_41
+  4  2     2.94534470E-01   # N_42
+  4  3     6.50435881E-01   # N_43
+  4  4    -6.88462993E-01   # N_44
+#
+BLOCK UMIX  # Chargino Mixing Matrix U
+  1  1     9.15480281E-01   # U_11
+  1  2    -4.02362840E-01   # U_12
+  2  1     4.02362840E-01   # U_21
+  2  2     9.15480281E-01   # U_22
+#
+BLOCK VMIX  # Chargino Mixing Matrix V
+  1  1     9.82636204E-01   # V_11
+  1  2    -1.85542692E-01   # V_12
+  2  1     1.85542692E-01   # V_21
+  2  2     9.82636204E-01   # V_22
+#
+BLOCK STOPMIX  # Stop Mixing Matrix
+  1  1     5.78881371E-01   # cos(theta_t)
+  1  2     8.15411772E-01   # sin(theta_t)
+  2  1    -8.15411772E-01   # -sin(theta_t)
+  2  2     5.78881371E-01   # cos(theta_t)
+#
+BLOCK SBOTMIX  # Sbottom Mixing Matrix
+  1  1     9.66726392E-01   # cos(theta_b)
+  1  2     2.55812594E-01   # sin(theta_b)
+  2  1    -2.55812594E-01   # -sin(theta_b)
+  2  2     9.66726392E-01   # cos(theta_b)
+#
+BLOCK STAUMIX  # Stau Mixing Matrix
+  1  1     4.51419848E-01   # cos(theta_tau)
+  1  2     8.92311672E-01   # sin(theta_tau)
+  2  1    -8.92311672E-01   # -sin(theta_tau)
+  2  2     4.51419848E-01   # cos(theta_tau)
+#
+BLOCK ALPHA  # Higgs mixing
+          -1.13676047E-01   # Mixing angle in the neutral Higgs boson sector
+#
+BLOCK HMIX Q=  2.90528802E+02  # DRbar Higgs Parameters
+         1     3.05599351E+02   # mu(Q)MSSM
+#
+BLOCK AU Q=  2.90528802E+02  # The trilinear couplings
+  1  1     0.00000000E+00   # A_u(Q) DRbar
+  2  2     0.00000000E+00   # A_c(Q) DRbar
+  3  3    -4.46245994E+02   # A_t(Q) DRbar
+#
+BLOCK AD Q=  2.90528802E+02  # The trilinear couplings
+  1  1     0.00000000E+00   # A_d(Q) DRbar
+  2  2     0.00000000E+00   # A_s(Q) DRbar
+  3  3    -8.28806503E+02   # A_b(Q) DRbar
+#
+BLOCK AE Q=  2.90528802E+02  # The trilinear couplings
+  1  1     0.00000000E+00   # A_e(Q) DRbar
+  2  2     0.00000000E+00   # A_mu(Q) DRbar
+  3  3    -4.92306701E+02   # A_tau(Q) DRbar
+#
+BLOCK MSOFT Q=  2.90528802E+02  # The soft SUSY breaking masses at the scale Q
+         1     6.39136864E+01   # M_1(Q)
+         2     1.22006983E+02   # M_2(Q)
+         3     3.90619532E+02   # M_3(Q)
+        21     4.42860395E+04   # mH1^2(Q)
+        22    -9.76585434E+04   # mH2^2(Q)
+        31     2.26648170E+02   # meL(Q)
+        32     2.26648170E+02   # mmuL(Q)
+        33     2.24355944E+02   # mtauL(Q)
+        34     2.08394096E+02   # meR(Q)
+        35     2.08394096E+02   # mmuR(Q)
+        36     2.03337218E+02   # mtauR(Q)
+        41     4.08594291E+02   # mqL1(Q)
+        42     4.08594291E+02   # mqL2(Q)
+        43     3.46134575E+02   # mqL3(Q)
+        44     3.98943379E+02   # muR(Q)
+        45     3.98943379E+02   # mcR(Q)
+        46     2.58021672E+02   # mtR(Q)
+        47     3.95211849E+02   # mdR(Q)
+        48     3.95211849E+02   # msR(Q)
+        49     3.90320031E+02   # mbR(Q)
+#
+DECAY    1000022    %E               # neutralino decays
+#   BR          NDA  ID1   ID2
+    1.0E00      2    1000039    22   # BR(~chi_10 -> ~gravitino ~photon)
+''' % (WIDTH)
+
+import FWCore.ParameterSet.Config as cms
+
+from Configuration.Generator.Pythia8CommonSettings_cfi import *
+from Configuration.Generator.Pythia8CUEP8M1Settings_cfi import *
+
+generator = cms.EDFilter('Pythia8GeneratorFilter',
+    comEnergy = cms.double(13000.0),
+    filterEfficiency = cms.untracked.double(1.0),
+    maxEventsToPrint = cms.untracked.int32(1),
+    pythiaHepMCVerbosity = cms.untracked.bool(False),
+    pythiaPylistVerbosity = cms.untracked.int32(0),
+    SLHATableForPythia8 = cms.string(SLHA_TABLE),
+    PythiaParameters = cms.PSet(
+        pythia8CommonSettingsBlock,
+        pythia8CUEP8M1SettingsBlock,
+        processParameters = cms.vstring(
+            'SUSY:gg2squarkantisquark = on',
+            'SUSY:qqbar2squarkantisquark = on',
+            'SUSY:qqbar2chi0chi0 = on',
+            'SUSY:idA = 1000022',
+            'SUSY:idB = 1000022',
+            '1000022:tau0 = %f' % CTAU,
+            ),
+        parameterSets = cms.vstring(
+            'pythia8CommonSettings',
+            'pythia8CUEP8M1Settings',
+            'processParameters',
+            ),
+        ),
+    )
---
+++ NeuNeuToPhoGrav_CTau5000mm_92X.py
@@ -0,0 +1,141 @@
+# Auto generated configuration file
+# using: 
+# Revision: 1.19 
+# Source: /local/reps/CMSSW/CMSSW/Configuration/Applications/python/ConfigBuilder.py,v 
+# with command line options: Configuration/GenProduction/python/ThirteenTeV/NeutralinoNeutralinoToGravitinoGravitinoPhotonPhoton_Tau5000mm_13TeV-pythia8_cff.py --fileout file:NeuNeuToPhoGrav_CTau5000mm.root --mc --eventcontent RAWSIM --datatier GEN-SIM --conditions 92X_upgrade2017_realistic_v7 --beamspot Realistic25ns13TeVEarly2017Collision --step GEN,SIM --nThreads 4 --geometry DB:Extended --era Run2_2017 --python_filename NeuNeuToPhoGrav_CTau5000mm.py --no_exec --customise Configuration/DataProcessing/Utils.addMonitoring -n 33
+import FWCore.ParameterSet.Config as cms
+
+from Configuration.StandardSequences.Eras import eras
+
+process = cms.Process('SIM',eras.Run2_2017)
+
+# import of standard configurations
+process.load('Configuration.StandardSequences.Services_cff')
+process.load('SimGeneral.HepPDTESSource.pythiapdt_cfi')
+process.load('FWCore.MessageService.MessageLogger_cfi')
+process.load('Configuration.EventContent.EventContent_cff')
+process.load('SimGeneral.MixingModule.mixNoPU_cfi')
+process.load('Configuration.StandardSequences.GeometryRecoDB_cff')
+process.load('Configuration.StandardSequences.GeometrySimDB_cff')
+process.load('Configuration.StandardSequences.MagneticField_cff')
+process.load('Configuration.StandardSequences.Generator_cff')
+process.load('IOMC.EventVertexGenerators.VtxSmearedRealistic25ns13TeVEarly2017Collision_cfi')
+process.load('GeneratorInterface.Core.genFilterSummary_cff')
+process.load('Configuration.StandardSequences.SimIdeal_cff')
+process.load('Configuration.StandardSequences.EndOfProcess_cff')
+process.load('Configuration.StandardSequences.FrontierConditions_GlobalTag_cff')
+
+process.maxEvents = cms.untracked.PSet(
+    input = cms.untracked.int32(33)
+)
+
+# Input source
+process.source = cms.Source("EmptySource")
+
+process.options = cms.untracked.PSet(
+
+)
+
+# Production Info
+process.configurationMetadata = cms.untracked.PSet(
+    annotation = cms.untracked.string('Configuration/GenProduction/python/ThirteenTeV/NeutralinoNeutralinoToGravitinoGravitinoPhotonPhoton_Tau5000mm_13TeV-pythia8_cff.py nevts:33'),
+    name = cms.untracked.string('Applications'),
+    version = cms.untracked.string('$Revision: 1.19 $')
+)
+
+# Output definition
+
+process.RAWSIMoutput = cms.OutputModule("PoolOutputModule",
+    SelectEvents = cms.untracked.PSet(
+        SelectEvents = cms.vstring('generation_step')
+    ),
+    compressionAlgorithm = cms.untracked.string('LZMA'),
+    compressionLevel = cms.untracked.int32(9),
+    dataset = cms.untracked.PSet(
+        dataTier = cms.untracked.string('GEN-SIM'),
+        filterName = cms.untracked.string('')
+    ),
+    eventAutoFlushCompressedSize = cms.untracked.int32(20971520),
+    fileName = cms.untracked.string('file:NeuNeuToPhoGrav_CTau5000mm.root'),
+    outputCommands = process.RAWSIMEventContent.outputCommands,
+    splitLevel = cms.untracked.int32(0)
+)
+
+# Additional output definition
+
+# Other statements
+process.XMLFromDBSource.label = cms.string("Extended")
+process.genstepfilter.triggerConditions=cms.vstring("generation_step")
+from Configuration.AlCa.GlobalTag import GlobalTag
+process.GlobalTag = GlobalTag(process.GlobalTag, '92X_upgrade2017_realistic_v7', '')
+
+process.generator = cms.EDFilter("Pythia8GeneratorFilter",
+    PythiaParameters = cms.PSet(
+        parameterSets = cms.vstring('pythia8CommonSettings', 
+            'pythia8CUEP8M1Settings', 
+            'processParameters'),
+        processParameters = cms.vstring('SUSY:gg2squarkantisquark = on', 
+            'SUSY:qqbar2squarkantisquark = on', 
+            'SUSY:qqbar2chi0chi0 = on', 
+            'SUSY:idA = 1000022', 
+            'SUSY:idB = 1000022', 
+            '1000022:tau0 = 5000.000000'),
+        pythia8CUEP8M1Settings = cms.vstring('Tune:pp 14', 
+            'Tune:ee 7', 
+            'MultipartonInteractions:pT0Ref=2.4024', 
+            'MultipartonInteractions:ecmPow=0.25208', 
+            'MultipartonInteractions:expPow=1.6'),
+        pythia8CommonSettings = cms.vstring('Tune:preferLHAPDF = 2', 
+            'Main:timesAllowErrors = 10000', 
+            'Check:epTolErr = 0.01', 
+            'Beams:setProductionScalesFromLHEF = off', 
+            'SLHA:keepSM = on', 
+            'SLHA:minMassSM = 1000.', 
+            'ParticleDecays:limitTau0 = on', 
+            'ParticleDecays:tau0Max = 10', 
+            'ParticleDecays:allowPhotonRadiation = on')
+    ),
+    SLHATableForPythia8 = cms.string('\nBLOCK SPINFO      # Spectrum calculator information\n    1    Minimal  # spectrum calculator\n    2    1.0.0    # version number\n\nBLOCK MODSEL      # Model selection\n    1    1        #\n\nBlock MINPAR  # Input parameters\n    3    1.00000000E+00  # tanb at m_Z   \n#\nBlock SMINPUTS  # SM parameters\n         1     1.27931277E+02  # alpha_em^-1(MZ)^MSbar\n         2     1.16639000E-05  # G_mu [GeV^-2]\n         3     1.17200000E-01  # alpha_s(MZ)^MSbar\n         4     9.11876000E+01  # m_Z(pole)\n         5     4.20000000E+00  # m_b(m_b), MSbar\n         6     1.74300000E+02  # m_t(pole)\n         7     1.77700000E+00  # m_tau(pole)\n\nBLOCK MASS        # Mass Spectrum\n#   PDG code   mass  particle\n        24     8.04009772E+01  # W+\n        25     125  # h0\n        35     10.0E+4  # H0\n        36     10.0E+4  # A0\n        37     10.0E+4  # H+\n   1000001     10.0E+4  # ~d_L\n   2000001     10.0E+4  # ~d_R\n   1000002     10.0E+4  # ~u_L\n   2000002     10.0E+4  # ~u_R\n   1000003     10.0E+4  # ~s_L\n   2000003     10.0E+4  # ~s_R\n   1000004     10.0E+4  # ~c_L\n   2000004     10.0E+4  # ~c_R\n   1000005     10.0E+4  # ~b_1\n   2000005     10.0E+4  # ~b_2\n   1000006     10.0E+4  # ~t_1 \n   2000006     10.0E+4  # ~t_2\n   1000011     10.0E+4  # ~e_L-\n   2000011     10.0E+4  # ~e_R-\n   1000012     10.0E+4  # ~nu_eL\n   1000013     10.0E+4  # ~mu_L-\n   2000013     10.0E+4  # ~mu_R-\n   1000014     10.0E+4  # ~nu_muL\n   1000015     10.0E+4  # ~tau_1-\n   2000015     10.0E+4  # ~tau_2-\n   1000016     10.0E+4  # ~nu_tauL\n   1000021     10.0E+4  # ~g \n   1000022     1000.0    # ~chi_10 \n   1000023     10.0E+4  # ~chi_20 \n   1000025     10.0E+4  # ~chi_30\n   1000035     10.0E+4  # ~chi_40\n   1000024     10.0E+4  # ~chi_1+ \n   1000037     10.0E+4  # ~chi_2+\n   1000039     1# ~G\n#\nBLOCK NMIX  # Neutralino Mixing Matrix\n  1  1     9.79183656E-01   # N_11\n  1  2    -8.70017948E-02   # N_12\n  1  3     1.75813037E-01   # N_13\n  1  4    -5.21520034E-02   # N_14\n  2  1     1.39174513E-01   # N_21\n  2  2     9.44472080E-01   # N_22\n  2  3    -2.71658234E-01   # N_23\n  2  4     1.21674770E-01   # N_24\n  3  1    -7.50233573E-02   # N_31\n  3  2     1.16844446E-01   # N_32\n  3  3     6.87186106E-01   # N_33\n  3  4     7.13087741E-01   # N_34\n  4  1    -1.27284400E-01   # N_41\n  4  2     2.94534470E-01   # N_42\n  4  3     6.50435881E-01   # N_43\n  4  4    -6.88462993E-01   # N_44\n#\nBLOCK UMIX  # Chargino Mixing Matrix U\n  1  1     9.15480281E-01   # U_11\n  1  2    -4.02362840E-01   # U_12\n  2  1     4.02362840E-01   # U_21\n  2  2     9.15480281E-01   # U_22\n#\nBLOCK VMIX  # Chargino Mixing Matrix V\n  1  1     9.82636204E-01   # V_11\n  1  2    -1.85542692E-01   # V_12\n  2  1     1.85542692E-01   # V_21\n  2  2     9.82636204E-01   # V_22\n#\nBLOCK STOPMIX  # Stop Mixing Matrix\n  1  1     5.78881371E-01   # cos(theta_t)\n  1  2     8.15411772E-01   # sin(theta_t)\n  2  1    -8.15411772E-01   # -sin(theta_t)\n  2  2     5.78881371E-01   # cos(theta_t)\n#\nBLOCK SBOTMIX  # Sbottom Mixing Matrix\n  1  1     9.66726392E-01   # cos(theta_b)\n  1  2     2.55812594E-01   # sin(theta_b)\n  2  1    -2.55812594E-01   # -sin(theta_b)\n  2  2     9.66726392E-01   # cos(theta_b)\n#\nBLOCK STAUMIX  # Stau Mixing Matrix\n  1  1     4.51419848E-01   # cos(theta_tau)\n  1  2     8.92311672E-01   # sin(theta_tau)\n  2  1    -8.92311672E-01   # -sin(theta_tau)\n  2  2     4.51419848E-01   # cos(theta_tau)\n#\nBLOCK ALPHA  # Higgs mixing\n          -1.13676047E-01   # Mixing angle in the neutral Higgs boson sector\n#\nBLOCK HMIX Q=  2.90528802E+02  # DRbar Higgs Parameters\n         1     3.05599351E+02   # mu(Q)MSSM\n#\nBLOCK AU Q=  2.90528802E+02  # The trilinear couplings\n  1  1     0.00000000E+00   # A_u(Q) DRbar\n  2  2     0.00000000E+00   # A_c(Q) DRbar\n  3  3    -4.46245994E+02   # A_t(Q) DRbar\n#\nBLOCK AD Q=  2.90528802E+02  # The trilinear couplings\n  1  1     0.00000000E+00   # A_d(Q) DRbar\n  2  2     0.00000000E+00   # A_s(Q) DRbar\n  3  3    -8.28806503E+02   # A_b(Q) DRbar\n#\nBLOCK AE Q=  2.90528802E+02  # The trilinear couplings\n  1  1     0.00000000E+00   # A_e(Q) DRbar\n  2  2     0.00000000E+00   # A_mu(Q) DRbar\n  3  3    -4.92306701E+02   # A_tau(Q) DRbar\n#\nBLOCK MSOFT Q=  2.90528802E+02  # The soft SUSY breaking masses at the scale Q\n         1     6.39136864E+01   # M_1(Q)\n         2     1.22006983E+02   # M_2(Q)\n         3     3.90619532E+02   # M_3(Q)\n        21     4.42860395E+04   # mH1^2(Q)\n        22    -9.76585434E+04   # mH2^2(Q)\n        31     2.26648170E+02   # meL(Q)\n        32     2.26648170E+02   # mmuL(Q)\n        33     2.24355944E+02   # mtauL(Q)\n        34     2.08394096E+02   # meR(Q)\n        35     2.08394096E+02   # mmuR(Q)\n        36     2.03337218E+02   # mtauR(Q)\n        41     4.08594291E+02   # mqL1(Q)\n        42     4.08594291E+02   # mqL2(Q)\n        43     3.46134575E+02   # mqL3(Q)\n        44     3.98943379E+02   # muR(Q)\n        45     3.98943379E+02   # mcR(Q)\n        46     2.58021672E+02   # mtR(Q)\n        47     3.95211849E+02   # mdR(Q)\n        48     3.95211849E+02   # msR(Q)\n        49     3.90320031E+02   # mbR(Q)\n#\nDECAY    1000022    3.940000E-17               # neutralino decays\n#   BR          NDA  ID1   ID2\n    1.0E00      2    1000039    22   # BR(~chi_10 -> ~gravitino ~photon)\n'),
+    comEnergy = cms.double(13000.0),
+    filterEfficiency = cms.untracked.double(1.0),
+    maxEventsToPrint = cms.untracked.int32(1),
+    pythiaHepMCVerbosity = cms.untracked.bool(False),
+    pythiaPylistVerbosity = cms.untracked.int32(0)
+)
+
+
+# Path and EndPath definitions
+process.generation_step = cms.Path(process.pgen)
+process.simulation_step = cms.Path(process.psim)
+process.genfiltersummary_step = cms.EndPath(process.genFilterSummary)
+process.endjob_step = cms.EndPath(process.endOfProcess)
+process.RAWSIMoutput_step = cms.EndPath(process.RAWSIMoutput)
+
+# Schedule definition
+process.schedule = cms.Schedule(process.generation_step,process.genfiltersummary_step,process.simulation_step,process.endjob_step,process.RAWSIMoutput_step)
+from PhysicsTools.PatAlgos.tools.helpers import associatePatAlgosToolsTask
+associatePatAlgosToolsTask(process)
+
+#Setup FWK for multithreaded
+process.options.numberOfThreads=cms.untracked.uint32(4)
+process.options.numberOfStreams=cms.untracked.uint32(0)
+# filter all path with the production filter sequence
+for path in process.paths:
+	getattr(process,path)._seq = process.generator * getattr(process,path)._seq 
+
+# customisation of the process.
+
+# Automatic addition of the customisation function from Configuration.DataProcessing.Utils
+from Configuration.DataProcessing.Utils import addMonitoring 
+
+#call to customisation function addMonitoring imported from Configuration.DataProcessing.Utils
+process = addMonitoring(process)
+
+# End of customisation functions
+
+# Customisation from command line
+
+# Add early deletion of temporary data products to reduce peak memory need
+from Configuration.StandardSequences.earlyDeleteSettings_cff import customiseEarlyDelete
+process = customiseEarlyDelete(process)
+# End adding early deletion
